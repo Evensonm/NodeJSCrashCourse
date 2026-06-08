8f8ede6b09fd2aed65bc47fd7a414897e6c39710
evensonm <evensonm1@gmail.com>
2026-06-08 15:22:41 -0400

    New changes

---
+++ server.js
@@ -0,0 +1,30 @@
+import http from 'http';
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+    // res.write('Hello World');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+
+    res.writeHead(200, { 'Content-Type': 'text/html'})
+
+    res.end('<h1> Hello World</h1>');
+
+    // res.end(JSON.stringify({message: 'Server Error'}));
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+});
+
+// new line commented
+
+
+
+// run "node server" to start server
+// server viewable on browser at localhost:PORT
+
+// "start": "node server"
+// "dev": "node server.js"
+
+// - D on npn install to make a DEV dependency and not for production
